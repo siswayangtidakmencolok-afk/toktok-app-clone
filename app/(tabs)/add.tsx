6a2aefcc9f21877d2c401e400026ac832aae0297
fhazwan <siswayangtidakmencolok@gmail.com>
2026-04-21 12:56:31 +0700

    tambahin add video and post

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,12 +1,35 @@
 // app/(tabs)/add.tsx
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function AddScreen() {
 
-  const handlePickVideo = () => {
-    console.log('Pilih video ditekan');
-    // TODO: buka file picker untuk pilih video
+  const SelectVideoFile = async () => {
+    // No permissions request is necessary for launching the image library.
+    // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
+    // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
+    // so the app users aren't surprised by a system dialog after picking a video.
+    // See "Invoke permissions for videos" sub section for more details.
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert('Permission required', 'Permission to access the media library is required.');
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      console.log('Selected video URI:', result.assets[0].uri);
+      setImage(result.assets[0].uri);
+    }
   };
 
   return (
@@ -30,8 +53,8 @@ export default function AddScreen() {
 
       {/* Tombol pilih video */}
       <TouchableOpacity
+        onPress={SelectVideoFile}
         style={styles.button}
-        onPress={handlePickVideo}
       >
         <Text style={styles.buttonText}>
           Pick Video From Gallery
